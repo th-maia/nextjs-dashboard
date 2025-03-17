7d804be2ceae262d1a14fb1e68e85ddc6a35dbf5
th-maia <thiago.maia@live.com>
2025-03-17 18:11:47 -0300

    auth page finished

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,9 +4,31 @@ import { z } from 'zod';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
